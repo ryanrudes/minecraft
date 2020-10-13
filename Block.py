22b4f17c9fd941bbf06c19db921265da266c55b1
Ryan Rudes <ryanrudes@gmail.com>
2020-10-13 15:53:58 -0400

    Create Block.py

---
+++ Block.py
@@ -0,0 +1,21 @@
+import colors
+
+class Block:
+    def __init__(self, color):
+        self.color = color
+
+
+DANDELION = Block(Color.PLANT)
+ROSE = Block(Color.PLANT)
+MUSHROOM_RED = Block(Color.RED)
+MUSHROOM_BROWN = Block(Color.BROWN)
+AIR = Block(Color.NONE)
+LEAVES = Block(Color.PLANT)
+TRUNK = Block(Color.WOOD)
+LAVA = Block(Color.FIRE)
+STONE = Block(Color.STONE)
+DIRT = Block(Color.DIRT)
+WATER = Block(Color.WATER)
+GRAVEL = Block(Color.STONE)
+SAND = Block(Color.SAND)
+GRASS = Block(Color.GRASS)
