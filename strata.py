8991c80e44b68659fff341ab6a721ccda27c4ae2
Ryan Rudes <ryanrudes@gmail.com>
2020-10-14 18:12:31 -0400

    Update strata.py

--- strata.py
+++ strata.py
@@ -1,10 +1,15 @@
+from objects import *
+from tqdm import tqdm
+import numpy as np
+import blocks
+
 def createStrata(heightMap,
                  height=64,
                  noise=lambda x, y: pnoise2(x, y, octaves = 8)):
 
     width, depth = heightMap.shape
   
-    cube = np.full((width, depth, height), AIR)
+    cube = np.full((width, depth, height), blocks.AIR)
     block = Chunk(cube, waterLevel = heightMap.metadata["waterLevel"])
 
     for x in tqdm(range(width), "Soiling..."):
@@ -15,13 +20,13 @@ def createStrata(heightMap,
 
             for y in range(height):
                 if y == 0:
-                    blockType = LAVA
+                    blockType = blocks.LAVA
                 elif y <= stoneTransition:
-                    blockType = STONE
+                    blockType = blocks.STONE
                 elif y <= dirtTransition:
-                    blockType = DIRT
+                    blockType = blocks.DIRT
                 else:
-                    blockType = AIR
+                    blockType = blocks.AIR
 
                 block[x, z, y] = blockType
 
