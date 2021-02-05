4b70c5bbacef490aa64f559949d688b1498272d1
Ryan Rudes <ryanrudes@gmail.com>
2021-02-05 08:18:39 -0500

    Add files via upload

---
+++ Java/Color.java
@@ -0,0 +1,13 @@
+public class Color {
+  int id, red, green, blue, alpha;
+  String name;
+
+  public Color (int id, String name, int red, int green, int blue, int alpha) {
+    this.id = id;
+    this.name = name;
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+    this.alpha = alpha;
+  }
+}
