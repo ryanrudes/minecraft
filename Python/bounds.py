b6a97a89ccd96b463285ba8fad938e3de107c5e6
Ryan Rudes <ryanrudes@gmail.com>
2021-02-05 08:57:47 -0500

    Delete Python directory

--- Python/bounds.py
+++
@@ -1,3 +0,0 @@
-def isWithinBounds(block, point, dim=2):
-    width, depth, height = block.shape
-    return point.x >= 0 and point.x < width and point.z >= 0 and point.z < depth and (dim == 2 or point.y >= 0 and point.y < height)
